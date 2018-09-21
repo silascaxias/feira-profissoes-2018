aaf3504414753ba0d38f68fbc322b264a3d42ad3
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-09-20 22:14:25 -0300

    Ajustes de layout

--- FeiraProfissoes/App_Start/BundleConfig.cs
+++ FeiraProfissoes/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace FeiraProfissoes
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js").Include("~/Scripts/animate.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +25,7 @@ namespace FeiraProfissoes
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", "~/Content/css/animate.css"));
         }
     }
 }
